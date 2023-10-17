26d2698b2112407f29a778ab644b7469a6cbc5d0
aleksandr04635 <lap04635@gmail.com>
2023-10-17 04:48:27 +0300

    ready to deploy2

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
   "https://technotes-xt3f.onrender.com/",
+  "https://technotes-xt3f.onrender.com/login",
   "http://localhost:3000",
   "https://www.dandrepairshop.com",
   "https://dandrepairshop.com",
